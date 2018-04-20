33ef1e7ce3e42d57c372ff0561ceeba8c2598002
Neanlitic <floduma2@hotmail.fr>
2018-04-20 10:52:58 +0200

    Add tab viewer

--- interface.py
+++ interface.py
@@ -1,12 +1,16 @@
 # http://apprendre-python.com/page-tkinter-interface-graphique-python-tutoriel
 # https://www.tutorialspoint.com/python/python_gui_programming.htm
+# https://docs.python.org/3/library/tkinter.ttk.html#notebook
+
 from tkinter import *
+from tkinter.ttk import *
 
 
 class Application(Frame):
-    # Example of function we can call
-    def say_hi(self):
-        print("hi there, everyone!")
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack(fill=BOTH)
+        self.createWidgets()
 
     def createWidgets(self):
         # --------- Example ---------
@@ -19,9 +23,22 @@ class Application(Frame):
         self.hi_there.pack({"side": "left"})
 
         # --------- Ours ---------
-        self.language = Checkbutton(self)
+        display = Notebook(self, name="nb")
+        display.pack(fill=BOTH, padx=2, pady=3)
 
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.pack()
-        self.createWidgets()
+        fen_user = Frame(display, name="fen_user")
+
+        language = Checkbutton(fen_user, text="yo")
+        language.pack(side=LEFT)
+
+        fen_visualiser = Frame(display, name="fen_visualiser")
+
+        language2 = Checkbutton(fen_visualiser, text="pyo")
+        language2.pack(side=RIGHT)
+
+        display.add(fen_user, text="Options")
+        display.add(fen_visualiser, text="Visualiseur")
+
+    # Example of function we can call
+    def say_hi(self):
+        print("hi there, everyone!")
