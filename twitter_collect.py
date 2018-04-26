ee8b6e5e14c5161e7a7000ab7e84a39f191dab3f
Neanlitic <floduma2@hotmail.fr>
2018-04-26 08:22:27 +0200

    Create a credential function out of github repository to avoid sending credential publicly

--- twitter_collect.py
+++ twitter_collect.py
@@ -3,15 +3,14 @@
 
 from twitter import *
 
+import credentials
+
 
 def connect_method():
-    # Variables that contains the user credentials to access Twitter API
-    ACCESS_TOKEN = '2390902586-sCozuoFv7hkgXOiqVmBenFaAbLhbPH0WzrhfXVr'
-    ACCESS_SECRET = 'MW1vksBifH9vvRXWzBy4WbPpzK51WnkjudbauymAsxl5H'
-    CONSUMER_KEY = '5gtIlLJA1AbWzTHbPWOUiVD0V'
-    CONSUMER_SECRET = 'eaYcylnb8q1cFvhgaAQm5dNYqd9CmVfnuUioiHktynLoau4Wuf'
-
-    return OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+    ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET = credentials.credentials()
+    return OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)  # you should create a credential function,
+    #  in a separate file not included to git, returning a tuple
+    # (ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 
 
 def search_sample(query):
@@ -33,14 +32,20 @@ def collect_tweet(nb_tweets=1):
 
     only_text = list()
     for msg in twitter_userstream.statuses.sample():  # infinite loop
-        nb_tweets -= 1
-        if msg['text']:
-            only_text.append(msg['text'])
+        print(msg)  # TODO : just for debugging purpose ; remove after not needed anymore
+        try:
+            if msg['text']:
+                only_text.append(msg['text'])
+                nb_tweets -= 1
+
+        except:
+            pass
 
         if not nb_tweets:  # to stop the loop when reaching the desired amount of tweets
             break
 
     # deal with the text
+    print(only_text)
     pass
 
 
