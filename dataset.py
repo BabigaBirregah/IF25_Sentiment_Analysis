ba2e7747586afde492968848917163b44a99c851
Neanlitic <floduma2@hotmail.fr>
2018-04-27 23:04:04 +0200

    Add a function to get some line of the csv file

---
+++ dataset.py
@@ -0,0 +1,24 @@
+number_tweets_each_file = 789314
+
+
+def get_line_file(number_line, file):
+    count_lines, list_tweets = 0, list()
+    for line in file:
+        count_lines += 1
+        list_tweets.append(line)
+        number_line -= 1
+        if not number_line:
+            break
+    return count_lines, list_tweets
+
+
+def get_some_sample(number_tweets):
+    with open("./CSV/Sentiment_analysis_dataset_1.csv", 'rb') as file_part1:
+        number_lines, sample_list = get_line_file(number_tweets, file_part1)
+        if number_tweets - number_lines:
+            with open("./CSV/Sentiment_analysis_dataset_2.csv", 'rb') as file_part2:
+                number_lines_2, sample_list_2 = get_line_file(number_tweets - number_lines, file_part2)
+                if number_tweets - (number_lines + number_lines_2):
+                    return number_tweets_each_file * 2, sample_list + sample_list_2
+                return number_tweets, sample_list + sample_list_2
+        return number_tweets, sample_list
